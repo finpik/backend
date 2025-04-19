7382e00d70c2b6dce8fe41b1d19832556b2b2a69
sanseongKo <tkstjd56@naver.com>
2025-04-19 19:32:26 +0900

    [feature/LP-1] 사용자 정보 받아오는 Oauth2 service 추가

---
+++ src/main/java/com/loanpick/auth/oauth/service/KakaoOAuth2UserService.java
@@ -0,0 +1,29 @@
+package com.loanpick.auth.oauth.service;
+
+import com.loanpick.auth.oauth.factory.OAuth2UserInfoFactory;
+import com.loanpick.auth.oauth.service.dto.CustomOAuth2User;
+import com.loanpick.auth.oauth.vo.OAuth2UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(provider, attributes);
+
+        return new CustomOAuth2User(attributes, userInfo.getEmail(), provider, userInfo.getUserId());
+    }
+}
