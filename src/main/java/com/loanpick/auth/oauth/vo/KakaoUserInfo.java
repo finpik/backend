eef3fa534f86b6f0a015a89581114f75b9d15ad4
sanseongKo <tkstjd56@naver.com>
2025-04-19 19:35:28 +0900

    [feature/LP-1] 실제 가져온 사용자 정보중 attribute를 열어 벤더사에 맞는 값을 리턴해주는 클래스 추가
    토큰 발급

---
+++ src/main/java/com/loanpick/auth/oauth/vo/KakaoUserInfo.java
@@ -0,0 +1,30 @@
+package com.loanpick.auth.oauth.vo;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class KakaoUserInfo implements OAuth2UserInfo{
+    private final Map<String, Object> attributes;
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object> account = mapper.convertValue(attributes.get("kakao_account"), new TypeReference<>(){});
+        return (String) account.get("email");
+    }
+
+    @Override
+    public String getUserId() {
+        return attributes.get("id").toString();
+    }
+}
---
+++ src/main/java/com/loanpick/auth/oauth/vo/OAuth2UserInfo.java
@@ -0,0 +1,7 @@
+package com.loanpick.auth.oauth.vo;
+
+public interface OAuth2UserInfo {
+    String getProviderId();
+    String getEmail();
+    String getUserId();
+}
