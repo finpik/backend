a0905cf47566f6be2ef75d291880acfb26527f17
sanseongKo <tkstjd56@naver.com>
2025-04-21 00:24:32 +0900

    [feature/LP-2] 실패 ruleset 적용되는지 확인하는 코드

---
+++ src/test/java/com/loanpick/RuleSetTest.java
@@ -0,0 +1,18 @@
+package com.loanpick;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RuleSetTest {
+    @DisplayName("")
+    @Test
+    void test() {
+        //given
+
+        //when
+
+        //then
+        Assertions.assertEquals(1, 2);
+    }
+}
