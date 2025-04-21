ce3eef974a99967d879bcb9579780e3a767e59f0
sanseongKo <tkstjd56@naver.com>
2025-04-21 19:47:23 +0900

    [feature/LP-1] 가독성 증가를 위한 함수 추출

--- src/main/java/com/loanpick/auth/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/loanpick/auth/oauth/OAuth2SuccessHandler.java
@@ -2,7 +2,6 @@ package com.loanpick.auth.oauth;
 
 import com.loanpick.auth.oauth.service.dto.CustomOAuth2User;
 import com.loanpick.auth.oauth.jwt.JwtProvider;
-import com.loanpick.user.entity.Gender;
 import com.loanpick.user.entity.User;
 import com.loanpick.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,6 +19,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
+    private static final int FIVE_MINUTE = 300_000;
 
     public OAuth2SuccessHandler(JwtProvider jwtProvider, UserRepository userRepository) {
         this.jwtProvider = jwtProvider;
@@ -29,30 +29,35 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(
         HttpServletRequest request, HttpServletResponse response, Authentication authentication
-    ) {
+    ) throws IOException {
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getEmail();
 
         Optional<User> user = userRepository.findByEmail(email);
 
         if (user.isPresent()) {
-            Date now = new Date();
-            Date expiry = new Date(now.getTime() + 3600_000); // 1시간
-            // JWT 발급
-            String jwt = jwtProvider.createToken(user.get(), now, expiry);
-
-            // JSON 응답
-            response.setHeader("Authorization", "Bearer " + jwt);
-            response.setStatus(HttpServletResponse.SC_OK);
+            responseSuccessLogin(response, getJwtToken(user.get()));
         } else {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseSignUpUser(response);
         }
     }
-}
 
-/**
- * 로그인, 회원가입을 위한 OAuth2 인가 인증은 항상 일어나야함
- * 카카오쪽 인가 인증이 안될 경우 403
- * 유저가 없다면 -> 회원가입을 해야하는 상태라면 404
- * 이미 있는 유저라면 token을 리턴해주는 걸로 결정
- */
+    private String getJwtToken(User user) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + FIVE_MINUTE);
+        return jwtProvider.createToken(user, now, expiry);
+    }
+
+    private void responseSuccessLogin(HttpServletResponse response, String jwt) {
+        // JSON 응답
+        response.setHeader(HttpHeaderValues.AUTHORIZATION, HttpHeaderValues.BEARER + jwt);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void responseSignUpUser(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        response.setContentType(HttpHeaderValues.APPLICATION_JSON);
+        response.setCharacterEncoding(HttpHeaderValues.UTF_8);
+        response.getWriter().write(ErrorMessage.SIGN_UP_NEEDED);
+    }
+}
