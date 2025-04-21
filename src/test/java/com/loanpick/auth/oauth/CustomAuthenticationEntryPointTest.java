0a15a9ba27e60c28a2ba59e764cccec02331c365
sanseongKo <tkstjd56@naver.com>
2025-04-21 20:05:24 +0900

    [feature/LP-1] 인증중 서버에러에 대한 에러 반환하는 테스트 코드 추가

---
+++ src/test/java/com/loanpick/auth/oauth/CustomAuthenticationEntryPointTest.java
@@ -0,0 +1,52 @@
+package com.loanpick.auth.oauth;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CustomAuthenticationEntryPointTest {
+
+    private final CustomAuthenticationEntryPoint entryPoint = new CustomAuthenticationEntryPoint();
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private AuthenticationException exception;
+
+    @DisplayName("인증중 서버 에러시 500 상태 코드와 에러 메시지를 반환한다.")
+    @Test
+    void commence() throws IOException {
+        // given
+        when(exception.getMessage()).thenReturn("Unauthorized");
+
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        // when
+        entryPoint.commence(request, response, exception);
+
+        // then
+        assertAll(
+            () -> verify(response).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR),
+            () -> verify(response).setContentType(HttpHeaderValues.APPLICATION_JSON),
+            () -> verify(writer).write(ErrorMessage.AUTHORIZATION_SERVER_ERROR)
+        );
+    }
+}
