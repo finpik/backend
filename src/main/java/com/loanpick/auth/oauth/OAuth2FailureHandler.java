b2ba3ccc8b7b1537b89b1d81636afa023d6bc8d7
sanseongKo <tkstjd56@naver.com>
2025-04-21 19:46:33 +0900

    [feature/LP-1] 인증 실패시 동작하는 Failure handler 추가, 인증시 서버 에러 처리 예외 핸들링 handler추가

---
+++ src/main/java/com/loanpick/auth/oauth/OAuth2FailureHandler.java
@@ -0,0 +1,25 @@
+package com.loanpick.auth.oauth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2FailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException exception
+    ) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(HttpHeaderValues.APPLICATION_JSON);
+        response.setCharacterEncoding(HttpHeaderValues.UTF_8);
+        response.getWriter().write(ErrorMessage.AUTHORIZATION_FAIL);
+    }
+}
