494f9cc6c021d8e873056f507d255b3aa5ee0ffe
sanseongKo <tkstjd56@naver.com>
2025-04-22 22:14:37 +0900

    [feature/LP-3] 레디스 설정 추가

---
+++ src/main/java/com/loanpick/config/redis/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.loanpick.config.redis;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
