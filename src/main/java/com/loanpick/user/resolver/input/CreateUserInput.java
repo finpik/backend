19823abdfe9a43e5266b20b1af59103f1354d14c
sanseongKo <tkstjd56@naver.com>
2025-04-24 23:43:08 +0900

    [feature/LP-3] user 생성 input valid처리 및 파라미터 추가
    provider, id 추가

--- src/main/java/com/loanpick/user/resolver/input/CreateUserInput.java
+++ src/main/java/com/loanpick/user/resolver/input/CreateUserInput.java
@@ -3,6 +3,20 @@ package com.loanpick.user.resolver.input;
 import java.time.LocalDate;
 
 import com.loanpick.user.entity.Gender;
+import com.loanpick.user.service.dto.CreateUserDto;
 
-public record CreateUserInput(String name, LocalDate dateOfBirth, Gender gender) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserInput(
+        @NotBlank(message = "이름을 넣어주세요.") @Size(min = 2, message = "이름은 최소 2글자 이상이어야 합니다.") String username,
+        @NotNull(message = "생일을 넣어주세요.") LocalDate dateOfBirth, Gender gender,
+        @NotBlank(message = "회원가입에 사용한 벤더사를 넣어주세요.") String provider,
+        @NotBlank(message = "사용자 벤더 아이디를 넣어주세요.") String id) {
+
+    public CreateUserDto toDto() {
+        return CreateUserDto.builder().username(username).dateOfBirth(dateOfBirth).gender(gender).provider(provider)
+                .id(id).build();
+    }
 }
