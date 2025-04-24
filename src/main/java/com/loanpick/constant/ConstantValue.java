c8c62fcfed33118048f399d99e7ebb879c28cc94
sanseongKo <tkstjd56@naver.com>
2025-04-24 23:35:44 +0900

    [feature/LP-3] 공통으로 사용되는 문자열 상수값으로 변경

---
+++ src/main/java/com/loanpick/constant/ConstantValue.java
@@ -0,0 +1,7 @@
+package com.loanpick.constant;
+
+public class ConstantValue {
+    public static final String EMAIL = "email";
+    public static final String KAKAO_ACCOUNT = "kakao_account";
+    public static final String USER_ID_FROM_VENDOR = "id";
+}
