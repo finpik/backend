4ba43b0e748ea69f7bbdbc7dd5397b305d1a8fd4
sanseongKo <tkstjd56@naver.com>
2025-04-24 23:41:59 +0900

    [feature/LP-3] 벤더사 문자열로 찾을 수 있는 코드 추가
    추후 naver, google등의 Oauth2 인증 방식 추가 대응

--- src/main/java/com/loanpick/user/entity/RegistrationType.java
+++ src/main/java/com/loanpick/user/entity/RegistrationType.java
@@ -1,5 +1,22 @@
 package com.loanpick.user.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum RegistrationType {
-    KAKAO
+    KAKAO("kakao");
+
+    private final String name;
+
+    public static RegistrationType fromName(String name) {
+        for (RegistrationType registrationType : RegistrationType.values()) {
+            if (registrationType.getName().equals(name)) {
+                return registrationType;
+            }
+        }
+
+        return null;
+    }
 }
