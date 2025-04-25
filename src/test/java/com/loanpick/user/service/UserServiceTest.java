5a124d099e3f86d3660553119a1fea7712b66a81
sanseongKo <tkstjd56@naver.com>
2025-04-25 22:13:50 +0900

    [feature/LP-3] 레디스 템플릿 설정

--- src/test/java/com/loanpick/user/service/UserServiceTest.java
+++ src/test/java/com/loanpick/user/service/UserServiceTest.java
@@ -8,11 +8,13 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import com.loanpick.config.redis.RedisConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.loanpick.redis.service.CustomRedisService;
@@ -24,6 +26,7 @@ import com.loanpick.user.service.dto.CreateUserDto;
 @SpringBootTest
 @ActiveProfiles("test") // test 설정 사용 시
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Import(RedisConfig.class)
 class UserServiceIntegrationTest {
 
     @Autowired
