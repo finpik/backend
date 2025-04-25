f1abe52d70a3d37e07da46c898a3fee174dda994
sanseongKo <tkstjd56@naver.com>
2025-04-25 21:31:39 +0900

    [feature/LP-3] github actions는 컴포넌트 스캔이 혹시 다를까...

--- src/test/java/com/loanpick/user/resolver/UserResolverTest.java
+++ src/test/java/com/loanpick/user/resolver/UserResolverTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@ComponentScan(basePackages = "com.loanpick")
 class CreateUserMutationTest {
 
     @Autowired
