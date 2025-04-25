fd5b8db9883684e48efbc0d74c646b02551a42c5
sanseongKo <tkstjd56@naver.com>
2025-04-25 23:25:25 +0900

    깃헙 테스트 실패 수정 테스트

--- src/main/java/com/loanpick/LoanPickApplication.java
+++ src/main/java/com/loanpick/LoanPickApplication.java
@@ -3,7 +3,7 @@ package com.loanpick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.loanpick")
 public class LoanPickApplication {
 
     public static void main(String[] args) {
