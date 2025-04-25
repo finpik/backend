28a3332b26a9e01bcaee8b05726dc842e29e2fa3
sanseongKo <tkstjd56@naver.com>
2025-04-25 22:31:07 +0900

    깃헙 액션스의 에러 내용이 다른 에러들 때문에 적게 나오는건지 확인

--- src/test/java/com/loanpick/user/resolver/UserResolverTest.java
+++ src/test/java/com/loanpick/user/resolver/UserResolverTest.java
@@ -1,56 +1,56 @@
-package com.loanpick.user.resolver;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Map;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-@SpringBootTest
-@ActiveProfiles("test")
-@AutoConfigureMockMvc
-class CreateUserMutationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @DisplayName("정상적 요청이 왔을 때 요청에 문제없이 동작한다.")
-    @Test
-    void createUserMutation() throws Exception {
-        // given
-        String graphqlQuery = """
-                    mutation {
-                        createUser(input: {
-                            username: "홍길동",
-                            dateOfBirth: "1990-01-01",
-                            gender: MALE,
-                            provider: "KAKAO",
-                            id: "123456"
-                        }) {
-                            username
-                        }
-                    }
-                """;
-
-        Map<String, String> requestBody = Map.of("query", graphqlQuery);
-        String jsonContent = objectMapper.writeValueAsString(requestBody);
-
-        // when
-        // then
-        mockMvc.perform(post("/graphql").contentType(MediaType.APPLICATION_JSON).content(jsonContent))
-                .andExpect(status().isOk());
-    }
-}
+//package com.loanpick.user.resolver;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import java.util.Map;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//@AutoConfigureMockMvc
+//class CreateUserMutationTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @DisplayName("정상적 요청이 왔을 때 요청에 문제없이 동작한다.")
+//    @Test
+//    void createUserMutation() throws Exception {
+//        // given
+//        String graphqlQuery = """
+//                    mutation {
+//                        createUser(input: {
+//                            username: "홍길동",
+//                            dateOfBirth: "1990-01-01",
+//                            gender: MALE,
+//                            provider: "KAKAO",
+//                            id: "123456"
+//                        }) {
+//                            username
+//                        }
+//                    }
+//                """;
+//
+//        Map<String, String> requestBody = Map.of("query", graphqlQuery);
+//        String jsonContent = objectMapper.writeValueAsString(requestBody);
+//
+//        // when
+//        // then
+//        mockMvc.perform(post("/graphql").contentType(MediaType.APPLICATION_JSON).content(jsonContent))
+//                .andExpect(status().isOk());
+//    }
+//}
