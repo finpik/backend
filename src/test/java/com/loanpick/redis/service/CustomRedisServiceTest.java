4eedddffed7b2fd8f1d2b865e7af774d98e38714
sanseongKo <tkstjd56@naver.com>
2025-04-25 22:36:59 +0900

    깃헙 테스트 실패 수정 테스트

--- src/test/java/com/loanpick/redis/service/CustomRedisServiceTest.java
+++ src/test/java/com/loanpick/redis/service/CustomRedisServiceTest.java
@@ -1,34 +1,34 @@
-//package com.loanpick.redis.service;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.time.Duration;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//class CustomRedisServiceTest {
-//    @Autowired
-//    CustomRedisService customRedisService;
-//
-//    @DisplayName("userId와 provider를 가지고 AccessToken을 저장한 것을 조회할 수 있다.")
-//    @Test
-//    void saveEmailForSignUp() {
-//        // given
-//        String userId = "userId";
-//        String providerId = "providerId";
-//        String accessToken = "accessToken";
-//
-//        // when
-//        customRedisService.saveEmailForSignUp(userId, providerId, accessToken, Duration.ofSeconds(10));
-//        String savedAccessToken = customRedisService.getEmailByCustomId(userId, providerId);
-//
-//        // then
-//        assertEquals(accessToken, savedAccessToken);
-//    }
-//}
+package com.loanpick.redis.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CustomRedisServiceTest {
+    @Autowired
+    CustomRedisService customRedisService;
+
+    @DisplayName("userId와 provider를 가지고 AccessToken을 저장한 것을 조회할 수 있다.")
+    @Test
+    void saveEmailForSignUp() {
+        // given
+        String userId = "userId";
+        String providerId = "providerId";
+        String accessToken = "accessToken";
+
+        // when
+        customRedisService.saveEmailForSignUp(userId, providerId, accessToken, Duration.ofSeconds(10));
+        String savedAccessToken = customRedisService.getEmailByCustomId(userId, providerId);
+
+        // then
+        assertEquals(accessToken, savedAccessToken);
+    }
+}
