3f06b338c5819ded9983fa9ce157bc141164a592
sanseongKo <tkstjd56@naver.com>
2025-04-27 22:18:41 +0900

    [feature/FP-4] 글로벌 에러처리를 위한 resolver 추가
    graphql은 200ok가 고정이기에 해당 리졸버를 통해 에러를 바디에 넣어서 리턴해야한다.

---
+++ src/main/java/com/loanpick/error/resolver/GlobalExceptionResolver.java
@@ -0,0 +1,36 @@
+package com.loanpick.error.resolver;
+
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+import com.loanpick.error.dispatcher.GraphQLExceptionHandlerDispatcher;
+import com.loanpick.error.handler.GraphQLExceptionHandler;
+
+import graphql.GraphQLError;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class GlobalExceptionResolver extends DataFetcherExceptionResolverAdapter {
+
+    private final GraphQLExceptionHandlerDispatcher handlerRegistry;
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        GraphQLExceptionHandler<Throwable> handler = findHandler(ex.getClass());
+        return handler.handle(ex, env);
+    }
+
+    private GraphQLExceptionHandler<Throwable> findHandler(Class<? extends Throwable> exceptionType) {
+        GraphQLExceptionHandler<? extends Throwable> handler = handlerRegistry.getHandler(exceptionType);
+
+        if (handler == null) {
+            throw new RuntimeException("No suitable handler found for exception type: " + exceptionType.getName());
+        }
+
+        @SuppressWarnings("unchecked")
+        GraphQLExceptionHandler<Throwable> castedHandler = (GraphQLExceptionHandler<Throwable>) handler;
+        return castedHandler;
+    }
+}
