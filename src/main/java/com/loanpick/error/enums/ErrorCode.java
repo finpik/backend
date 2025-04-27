95caf4f2747a52cf006ee0e82b003f2ebba643d2
sanseongKo <tkstjd56@naver.com>
2025-04-27 22:24:13 +0900

    [feature/FP-4] 에러 리스폰스에 필요한 또는 커스텀 에러 처리를 위한 에러 클래스 추가

---
+++ src/main/java/com/loanpick/error/enums/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.loanpick.error.enums;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // --------- 글로벌 에러처리 시작-------
+    INTERNAL_SERVER_ERROR("서버 에러", "INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR),
+    CONSTRAINT_VIOLATION_EXCEPTION("입력값 검증 실패", "VALIDATION_ERROR", HttpStatus.BAD_REQUEST),
+    BIND_EXCEPTION("입력값 검증 실패", "VALIDATION_ERROR", HttpStatus.BAD_REQUEST),
+    METHOD_ARGUMENT_NOT_VALID_EXCEPTION("입력값 검증 실패", "VALIDATION_ERROR", HttpStatus.BAD_REQUEST),
+    // --------- 글로벌 에러처리 끝-------
+    ;
+
+    private final String message;
+    private final String code;
+    private final HttpStatus status;
+}
