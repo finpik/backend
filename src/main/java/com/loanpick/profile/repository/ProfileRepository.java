665b4f56c0e426cb8f1398917d2fb19aa7f2c02b
sanseongKo <tkstjd56@naver.com>
2025-04-28 23:56:34 +0900

    [feature/FP-6] Profile repository 인터페이스 추가
    테스트 코드 추가

---
+++ src/main/java/com/loanpick/profile/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.loanpick.profile.repository;
+
+import com.loanpick.profile.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+}
