171d740fc98bb2dd1f594262bcd6e8aee136af4d
sanseongKo <tkstjd56@naver.com>
2025-04-28 15:27:29 +0900

    [feature/FP-4] 에러코드 네이밍 변경

--- src/main/java/com/loanpick/error/enums/ErrorCode.java
+++ src/main/java/com/loanpick/error/enums/ErrorCode.java
@@ -10,10 +10,13 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     // --------- 글로벌 에러처리 시작-------
     INTERNAL_SERVER_ERROR("서버 에러", "INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR),
-    CONSTRAINT_VIOLATION_EXCEPTION("입력값 검증 실패", "VALIDATION_ERROR", HttpStatus.BAD_REQUEST),
-    BIND_EXCEPTION("입력값 검증 실패", "VALIDATION_ERROR", HttpStatus.BAD_REQUEST),
-    METHOD_ARGUMENT_NOT_VALID_EXCEPTION("입력값 검증 실패", "VALIDATION_ERROR", HttpStatus.BAD_REQUEST),
+    CONSTRAINT_VIOLATION_EXCEPTION("입력값 검증 실패", "CONSTRAINT_VIOLATION_EXCEPTION", HttpStatus.BAD_REQUEST),
+    BIND_EXCEPTION("입력값 검증 실패", "BIND_EXCEPTION", HttpStatus.BAD_REQUEST),
+    METHOD_ARGUMENT_NOT_VALID_EXCEPTION("입력값 검증 실패", "METHOD_ARGUMENT_NOT_VALID_EXCEPTION", HttpStatus.BAD_REQUEST),
     // --------- 글로벌 에러처리 끝-------
+
+    //Test
+    TEST_EXCEPTION("test용 에러", "TEST_EXCEPTION", HttpStatus.BAD_REQUEST),
     ;
 
     private final String message;
