31da14eba4f28059a70de065890269688852cafb
sanseongKo <tkstjd56@naver.com>
2025-04-28 23:55:46 +0900

    [feature/FP-6] Profile Resolver 클래스 및 Input Result 추가
    테스트 코드 추가

--- src/main/java/com/loanpick/profile/resolver/ProfileResolver.java
+++ src/main/java/com/loanpick/profile/resolver/ProfileResolver.java
@@ -1,21 +1,38 @@
 package com.loanpick.profile.resolver;
 
+import com.loanpick.profile.entity.Profile;
+import com.loanpick.profile.service.ProfileService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 import com.loanpick.profile.resolver.input.CreateProfileInput;
 import com.loanpick.profile.resolver.result.ProfileResult;
 
 @Controller
+@RequiredArgsConstructor
 public class ProfileResolver implements ProfileApi {
+    private final ProfileService profileService;
 
     @Override
-    public ProfileResult createProfile(CreateProfileInput input) {
+    @MutationMapping
+    public ProfileResult createProfile(@Argument @Valid CreateProfileInput input) {
+        Profile profile = profileService.createProfile(input.toDto());
 
-        return null;
+        return ProfileResult.builder()
+            .purposeOfLoan(profile.getPurposeOfLoan())
+            .creditGradeStatus(profile.getCreditGradeStatus())
+            .loanProductUsageCount(profile.getLoanProductUsageCount())
+            .totalLoanUsageAmount(profile.getTotalLoanUsageAmount())
+            .desiredLoanAmount(profile.getDesiredLoanAmount())
+            .employmentStatus(profile.getEmploymentStatus())
+            .build();
     }
 
     @Override
-    public ProfileResult profileByUserId(long id) {
+    public ProfileResult profileByUserId(@Argument long id) {
         return null;
     }
 }
--- src/main/java/com/loanpick/profile/resolver/result/ProfileResult.java
+++ src/main/java/com/loanpick/profile/resolver/result/ProfileResult.java
@@ -1,5 +1,14 @@
 package com.loanpick.profile.resolver.result;
 
-public record ProfileResult(String purposeOfLoan, String job, String creditGradeStatus, int loanProductUsageCount,
-        int totalLoanUsageAmount, int desiredLoanAmount) {
+import com.loanpick.profile.entity.enums.CreditGradeStatus;
+import com.loanpick.profile.entity.enums.EmploymentStatus;
+import com.loanpick.profile.entity.enums.PurposeOfLoan;
+import lombok.Builder;
+
+@Builder
+public record ProfileResult(
+    PurposeOfLoan purposeOfLoan, EmploymentStatus employmentStatus,
+    CreditGradeStatus creditGradeStatus, int loanProductUsageCount,
+    int totalLoanUsageAmount, int desiredLoanAmount
+) {
 }
