0996dec58bd53e02bb3d1de42358415c7c4f93f0
sanseongKo <tkstjd56@naver.com>
2025-05-01 11:54:04 +0900

    [feature/FP-6] 프로필 조회 및 생성 파라미터 추가

--- src/main/java/com/loanpick/profile/resolver/ProfileApi.java
+++ src/main/java/com/loanpick/profile/resolver/ProfileApi.java
@@ -1,10 +1,17 @@
 package com.loanpick.profile.resolver;
 
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+
 import com.loanpick.profile.resolver.input.CreateProfileInput;
 import com.loanpick.profile.resolver.result.ProfileResult;
+import com.loanpick.user.entity.User;
+
+import jakarta.validation.Valid;
 
 public interface ProfileApi {
-    ProfileResult createProfile(CreateProfileInput input);
+    ProfileResult createProfile(@Argument @Valid CreateProfileInput input, User user);
 
-    ProfileResult profileByUserId(long id);
+    List<ProfileResult> profileByUserId(User user);
 }
