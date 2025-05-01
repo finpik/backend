3dd695d610432e3e04458683a4f7cecf210161e4
sanseongKo <tkstjd56@naver.com>
2025-05-01 11:57:07 +0900

    [feature/FP-6] 프로필 응답 순번 파라미터 추가

--- src/main/java/com/loanpick/profile/resolver/result/ProfileResult.java
+++ src/main/java/com/loanpick/profile/resolver/result/ProfileResult.java
@@ -1,5 +1,6 @@
 package com.loanpick.profile.resolver.result;
 
+import com.loanpick.profile.entity.Profile;
 import com.loanpick.profile.entity.enums.CreditGradeStatus;
 import com.loanpick.profile.entity.enums.EmploymentStatus;
 import com.loanpick.profile.entity.enums.PurposeOfLoan;
@@ -8,6 +9,15 @@ import lombok.Builder;
 
 @Builder
 public record ProfileResult(PurposeOfLoan purposeOfLoan, EmploymentStatus employmentStatus,
-        CreditGradeStatus creditGradeStatus, int loanProductUsageCount, int totalLoanUsageAmount,
-        int desiredLoanAmount) {
+        CreditGradeStatus creditGradeStatus, int loanProductUsageCount, int totalLoanUsageAmount, int desiredLoanAmount,
+        int seq) {
+
+    public static ProfileResult of(Profile profile) {
+        return ProfileResult.builder().purposeOfLoan(profile.getPurposeOfLoan())
+                .creditGradeStatus(profile.getCreditGradeStatus())
+                .loanProductUsageCount(profile.getLoanProductUsageCount())
+                .totalLoanUsageAmount(profile.getTotalLoanUsageAmount())
+                .desiredLoanAmount(profile.getDesiredLoanAmount()).employmentStatus(profile.getEmploymentStatus())
+                .seq(profile.getSeq()).build();
+    }
 }
