3d12a5ca37762ccca8ee2cfd8049ccab94f5626b
sanseongKo <tkstjd56@naver.com>
2025-05-01 12:05:27 +0900

    [feature/FP-6] 프로필 생성시 자동 프로필 순번 밸런싱 로직 추가

--- src/main/java/com/loanpick/profile/service/ProfileServiceImpl.java
+++ src/main/java/com/loanpick/profile/service/ProfileServiceImpl.java
@@ -1,14 +1,21 @@
 package com.loanpick.profile.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.loanpick.error.enums.ErrorCode;
+import com.loanpick.error.exception.BusinessException;
 import com.loanpick.profile.entity.Profile;
 import com.loanpick.profile.repository.ProfileRepository;
 import com.loanpick.profile.service.dto.CreateProfileDto;
+import com.loanpick.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
@@ -19,8 +26,9 @@ public class ProfileServiceImpl implements ProfileService {
     @Transactional
     public Profile createProfile(CreateProfileDto dto) {
         Profile entity = dto.toEntity();
-        List<Profile> profileList = profileRepository.findByUser(dto.user());
+        List<Profile> profileList = getProfileListBy(dto.user());
         validateProfileCountLimit(profileList);
+        balanceProfileSequence(profileList);
 
         return profileRepository.save(entity);
     }
@@ -36,6 +44,18 @@ public class ProfileServiceImpl implements ProfileService {
             throw new BusinessException(ErrorCode.EXCEEDING_PROFILE_COUNT_LIMIT);
         }
     }
+
+    private void balanceProfileSequence(List<Profile> profileList) {
+        if (profileList.isEmpty())
+            return;
+
+        profileList.forEach(Profile::balanceSequence);
+    }
+
+    private List<Profile> getProfileListBy(User user) {
+        return profileRepository.findByUser(user);
+    }
+
     private boolean isNotLessThanLimit(int count) {
         return count >= PROFILE_LIMIT_NUMBER;
     }
