78bfd8a8b8af6a53e3175c67d936937efb5702f0
sanseongKo <tkstjd56@naver.com>
2025-05-03 15:16:41 +0900

    [feature/FP-6] 프로필 엔티티 추가

--- src/test/java/com/loanpick/profile/entity/ProfileTest.java
+++ src/test/java/com/loanpick/profile/entity/ProfileTest.java
@@ -1,6 +1,7 @@
 package com.loanpick.profile.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDate;
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.loanpick.error.enums.ErrorCode;
 import com.loanpick.profile.entity.enums.CreditGradeStatus;
 import com.loanpick.profile.entity.enums.EmploymentForm;
 import com.loanpick.profile.entity.enums.EmploymentStatus;
@@ -24,37 +26,41 @@ class ProfileTest {
         int loanProductUsageCount = 2;
         int totalLoanUsageAmount = 5000000;
         int creditScore = 750;
-        int income = 60000000;
-        String workplaceName = "Sample Company";
         String profileName = "내 프로필";
         EmploymentForm employmentForm = EmploymentForm.FULL_TIME;
         LoanProductUsageStatus loanProductUsageStatus = LoanProductUsageStatus.USING;
         PurposeOfLoan purposeOfLoan = PurposeOfLoan.HOUSING;
-        EmploymentStatus employmentStatus = EmploymentStatus.EMPLOYEE;
+        EmploymentStatus employmentStatus = EmploymentStatus.SELF_EMPLOYED;
         CreditGradeStatus creditGradeStatus = CreditGradeStatus.UPPER;
-        LocalDate employmentDate = LocalDate.of(2020, 1, 1);
 
         // when
         Profile profile = Profile.builder().desiredLoanAmount(desiredLoanAmount)
                 .loanProductUsageCount(loanProductUsageCount).totalLoanUsageAmount(totalLoanUsageAmount)
-                .creditScore(creditScore).income(income).workplaceName(workplaceName).profileName(profileName)
-                .employmentForm(employmentForm).loanProductUsageStatus(loanProductUsageStatus)
-                .purposeOfLoan(purposeOfLoan).employmentStatus(employmentStatus).creditGradeStatus(creditGradeStatus)
-                .employmentDate(employmentDate).build();
+                .creditScore(creditScore).profileName(profileName).employmentForm(employmentForm)
+                .loanProductUsageStatus(loanProductUsageStatus).purposeOfLoan(purposeOfLoan)
+                .employmentStatus(employmentStatus).creditGradeStatus(creditGradeStatus).build();
 
         // then
         assertAll(() -> assertThat(profile.getDesiredLoanAmount()).isEqualTo(desiredLoanAmount),
                 () -> assertThat(profile.getLoanProductUsageCount()).isEqualTo(loanProductUsageCount),
                 () -> assertThat(profile.getTotalLoanUsageAmount()).isEqualTo(totalLoanUsageAmount),
                 () -> assertThat(profile.getCreditScore()).isEqualTo(creditScore),
-                () -> assertThat(profile.getIncome()).isEqualTo(income),
-                () -> assertThat(profile.getWorkplaceName()).isEqualTo(workplaceName),
                 () -> assertThat(profile.getProfileName()).isEqualTo(profileName),
                 () -> assertThat(profile.getEmploymentForm()).isEqualTo(employmentForm),
                 () -> assertThat(profile.getLoanProductUsageStatus()).isEqualTo(loanProductUsageStatus),
                 () -> assertThat(profile.getPurposeOfLoan()).isEqualTo(purposeOfLoan),
                 () -> assertThat(profile.getEmploymentStatus()).isEqualTo(employmentStatus),
-                () -> assertThat(profile.getCreditGradeStatus()).isEqualTo(creditGradeStatus),
-                () -> assertThat(profile.getEmploymentDate()).isEqualTo(employmentDate));
+                () -> assertThat(profile.getCreditGradeStatus()).isEqualTo(creditGradeStatus));
+    }
+
+    @DisplayName("프로필을 만들 때 EmploymentStatus가 EMPLOYEE일 때, 관련 필드가 없으면 에러가 발생한다.")
+    @Test
+    void validateInfoRelatedEmploymentStatus() {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> Profile.builder().employmentStatus(EmploymentStatus.EMPLOYEE)
+                .workplaceName("Sample Company").employmentDate(LocalDate.of(2020, 1, 1)).build())
+                .hasMessage(ErrorCode.INVALID_EMPLOYMENT_INFO.getMessage());
     }
 }
