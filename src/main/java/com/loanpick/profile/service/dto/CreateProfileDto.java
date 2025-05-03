f76c752c0fd39bf7bab292848001a6800713166e
sanseongKo <tkstjd56@naver.com>
2025-05-03 15:10:37 +0900

    [feature/FP-6] spotless 기능 off

--- src/main/java/com/loanpick/profile/service/dto/CreateProfileDto.java
+++ src/main/java/com/loanpick/profile/service/dto/CreateProfileDto.java
@@ -7,23 +7,50 @@ import com.loanpick.profile.entity.enums.CreditGradeStatus;
 import com.loanpick.profile.entity.enums.EmploymentForm;
 import com.loanpick.profile.entity.enums.EmploymentStatus;
 import com.loanpick.profile.entity.enums.LoanProductUsageStatus;
+import com.loanpick.profile.entity.enums.ProfileColor;
 import com.loanpick.profile.entity.enums.PurposeOfLoan;
 import com.loanpick.user.entity.User;
 
 import lombok.Builder;
 
+//@formatter:off
 @Builder
-public record CreateProfileDto(String workplaceName, EmploymentForm employmentForm, int income,
-        EmploymentStatus employmentStatus, LocalDate employmentDate, LoanProductUsageStatus loanProductUsageStatus,
-        int loanProductUsageCount, int totalLoanUsageAmount, PurposeOfLoan purposeOfLoan, int desiredLoanAmount,
-        CreditGradeStatus creditGradeStatus, int creditScore, String profileName, User user) {
+public record CreateProfileDto(
+    String workplaceName,
+    EmploymentForm employmentForm,
+    Integer income,
+    EmploymentStatus employmentStatus,
+    LocalDate employmentDate,
+    LoanProductUsageStatus loanProductUsageStatus,
+    Integer loanProductUsageCount,
+    Integer totalLoanUsageAmount,
+    PurposeOfLoan purposeOfLoan,
+    Integer desiredLoanAmount,
+    CreditGradeStatus creditGradeStatus,
+    Integer creditScore,
+    String profileName,
+    User user,
+    ProfileColor profileColor
+) {
     public Profile toEntity() {
-        return Profile.builder().desiredLoanAmount(desiredLoanAmount).loanProductUsageStatus(loanProductUsageStatus)
-                .totalLoanUsageAmount(totalLoanUsageAmount).creditScore(creditScore)
-                .loanProductUsageCount(loanProductUsageCount).creditGradeStatus(creditGradeStatus)
-                .creditGradeStatus(creditGradeStatus).loanProductUsageStatus(loanProductUsageStatus)
-                .purposeOfLoan(purposeOfLoan).workplaceName(workplaceName).employmentForm(employmentForm).income(income)
-                .profileName(profileName).employmentDate(employmentDate).employmentStatus(employmentStatus).user(user)
-                .build();
+        return Profile.builder()
+            .desiredLoanAmount(desiredLoanAmount)
+            .loanProductUsageStatus(loanProductUsageStatus)
+            .totalLoanUsageAmount(totalLoanUsageAmount)
+            .creditScore(creditScore)
+            .loanProductUsageCount(loanProductUsageCount)
+            .creditGradeStatus(creditGradeStatus)
+            .creditGradeStatus(creditGradeStatus)
+            .loanProductUsageStatus(loanProductUsageStatus)
+            .purposeOfLoan(purposeOfLoan)
+            .workplaceName(workplaceName)
+            .employmentForm(employmentForm)
+            .income(income)
+            .profileName(profileName)
+            .employmentDate(employmentDate)
+            .employmentStatus(employmentStatus)
+            .user(user)
+            .profileColor(profileColor)
+            .build();
     }
 }
