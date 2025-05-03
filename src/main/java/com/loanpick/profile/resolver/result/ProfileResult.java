0ee71871d114512a129166162a27f2ff6a325523
sanseongKo <tkstjd56@naver.com>
2025-05-03 15:08:24 +0900

    [feature/FP-6] spotless 기능 off

--- src/main/java/com/loanpick/profile/resolver/result/ProfileResult.java
+++ src/main/java/com/loanpick/profile/resolver/result/ProfileResult.java
@@ -3,21 +3,38 @@ package com.loanpick.profile.resolver.result;
 import com.loanpick.profile.entity.Profile;
 import com.loanpick.profile.entity.enums.CreditGradeStatus;
 import com.loanpick.profile.entity.enums.EmploymentStatus;
+import com.loanpick.profile.entity.enums.ProfileColor;
 import com.loanpick.profile.entity.enums.PurposeOfLoan;
 
 import lombok.Builder;
 
+//@formatter:off
 @Builder
-public record ProfileResult(long id, PurposeOfLoan purposeOfLoan, EmploymentStatus employmentStatus,
-        CreditGradeStatus creditGradeStatus, int loanProductUsageCount, int totalLoanUsageAmount, int desiredLoanAmount,
-        int seq, String profileName) {
+public record ProfileResult(
+    Long id,
+    PurposeOfLoan purposeOfLoan,
+    EmploymentStatus employmentStatus,
+    CreditGradeStatus creditGradeStatus,
+    Integer loanProductUsageCount,
+    Integer totalLoanUsageAmount,
+    Integer desiredLoanAmount,
+    Integer seq,
+    String profileName,
+    ProfileColor profileColor
+) {
 
     public static ProfileResult of(Profile profile) {
-        return ProfileResult.builder().id(profile.getId()).purposeOfLoan(profile.getPurposeOfLoan())
-                .creditGradeStatus(profile.getCreditGradeStatus())
-                .loanProductUsageCount(profile.getLoanProductUsageCount())
-                .totalLoanUsageAmount(profile.getTotalLoanUsageAmount())
-                .desiredLoanAmount(profile.getDesiredLoanAmount()).employmentStatus(profile.getEmploymentStatus())
-                .profileName(profile.getProfileName()).seq(profile.getSeq()).build();
+        return ProfileResult.builder()
+            .id(profile.getId())
+            .purposeOfLoan(profile.getPurposeOfLoan())
+            .creditGradeStatus(profile.getCreditGradeStatus())
+            .loanProductUsageCount(profile.getLoanProductUsageCount())
+            .totalLoanUsageAmount(profile.getTotalLoanUsageAmount())
+            .desiredLoanAmount(profile.getDesiredLoanAmount())
+            .employmentStatus(profile.getEmploymentStatus())
+            .profileName(profile.getProfileName())
+            .seq(profile.getSeq())
+            .profileColor(profile.getProfileColor())
+            .build();
     }
 }
