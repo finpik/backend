b920ce981801a943e9b633d442c36eaa3cefd105
sanseongKo <tkstjd56@naver.com>
2025-05-03 15:07:53 +0900

    [feature/FP-6] 프로필 수정 input 데이터 객체로 변환
    객체가 아니라면 초기값이 설정되는 문제가 있음 특히 int, long 이 0으로 변함

--- src/main/java/com/loanpick/profile/resolver/input/UpdateProfileInput.java
+++ src/main/java/com/loanpick/profile/resolver/input/UpdateProfileInput.java
@@ -19,24 +19,24 @@ import lombok.Builder;
 @Builder
 public record UpdateProfileInput(
     @NotNull(message = "프로필 id가 필요합니다.")
-    long id,
+    Long id,
     @NotNull(message = "직업을 선택해 주세요.")
     EmploymentStatus employmentStatus,
     String workplaceName,
     EmploymentForm employmentForm,
-    int income,
+    Integer income,
     LocalDate employmentDate,
     @NotNull(message = "대출 목적을 선택해 주세요.")
     PurposeOfLoan purposeOfLoan,
     @NotNull(message = "대출 희망 금액을 적어주세요.") @Min(value = 0, message = "대출 희망 금액은 0원 이상 작성해주세요.")
-    int desiredLoanAmount,
+    Integer desiredLoanAmount,
     @NotNull(message = "이용하는 서비스가 있는지 선택해주세요.")
     LoanProductUsageStatus loanProductUsageStatus,
     @NotNull(message = "현재 가입하신 대출 개수를 적어주세요.") @Min(value = 0, message = "대출 개수는 0개 이상 작성해주세요.")
-    int loanProductUsageCount,
+    Integer loanProductUsageCount,
     @NotNull(message = "현재 가입하신 모든 대출 금액을 적어주세요.") @Min(value = 0, message = "대출 금액은 0원 이상 작성해주세요.")
-    int totalLoanUsageAmount,
-    int creditScore,
+    Integer totalLoanUsageAmount,
+    Integer creditScore,
     CreditGradeStatus creditGradeStatus,
     @NotBlank(message = "작성하신 프로필의 이름을 만들어주세요.") @Size(min = 1, max = 14, message = "프로필 이름은 1자 이상 14자 이하로 작성해주세요.")
     String profileName
