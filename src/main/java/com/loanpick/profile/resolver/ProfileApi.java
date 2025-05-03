4260f3be2e1c9a634efefe8d810fb4616acbcd84
sanseongKo <tkstjd56@naver.com>
2025-05-03 15:10:30 +0900

    [feature/FP-6] 프로필 순번 수정, 색상 수정 api추가, interceptor에서 user를 받아 권한 검증

--- src/main/java/com/loanpick/profile/resolver/ProfileApi.java
+++ src/main/java/com/loanpick/profile/resolver/ProfileApi.java
@@ -3,8 +3,10 @@ package com.loanpick.profile.resolver;
 import java.util.List;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.ContextValue;
 
 import com.loanpick.profile.resolver.input.CreateProfileInput;
+import com.loanpick.profile.resolver.input.UpdateProfileColorInput;
 import com.loanpick.profile.resolver.input.UpdateProfileInput;
 import com.loanpick.profile.resolver.input.UpdateProfileSequenceInput;
 import com.loanpick.profile.resolver.result.ProfileResult;
@@ -13,11 +15,15 @@ import com.loanpick.user.entity.User;
 import jakarta.validation.Valid;
 
 public interface ProfileApi {
-    ProfileResult createProfile(@Argument @Valid CreateProfileInput input, User user);
+    ProfileResult createProfile(@Argument @Valid CreateProfileInput input, @ContextValue("user") User userInput);
 
-    List<ProfileResult> profileByUserId(User user);
+    List<ProfileResult> profileByUser(@ContextValue("user") User userInput);
 
     ProfileResult updateProfile(@Argument @Valid UpdateProfileInput input);
 
-    List<ProfileResult> updateProfileSequence(@Argument @Valid List<UpdateProfileSequenceInput> input, User user);
+    List<ProfileResult> updateProfileSequence(@Argument @Valid List<UpdateProfileSequenceInput> input,
+            @ContextValue("user") User userInput);
+
+    ProfileResult updateProfileColor(@Argument @Valid UpdateProfileColorInput input,
+            @ContextValue("user") User userInput);
 }
--- src/main/java/com/loanpick/profile/resolver/ProfileResolver.java
+++ src/main/java/com/loanpick/profile/resolver/ProfileResolver.java
@@ -3,17 +3,21 @@ package com.loanpick.profile.resolver;
 import java.util.List;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.ContextValue;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import com.loanpick.profile.entity.Profile;
 import com.loanpick.profile.resolver.input.CreateProfileInput;
+import com.loanpick.profile.resolver.input.UpdateProfileColorInput;
 import com.loanpick.profile.resolver.input.UpdateProfileInput;
 import com.loanpick.profile.resolver.input.UpdateProfileSequenceInput;
 import com.loanpick.profile.resolver.result.ProfileResult;
 import com.loanpick.profile.service.ProfileService;
 import com.loanpick.profile.service.dto.UpdateProfileSequenceDto;
 import com.loanpick.user.entity.User;
+import com.loanpick.util.UserUtil;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,20 +29,27 @@ public class ProfileResolver implements ProfileApi {
 
     @Override
     @MutationMapping
-    public ProfileResult createProfile(@Argument @Valid CreateProfileInput input, User user) {
+    public ProfileResult createProfile(@Argument @Valid CreateProfileInput input,
+            @ContextValue("user") User userInput) {
+        User user = UserUtil.require(userInput);
+
         Profile profile = profileService.createProfile(input.toDto(user));
 
         return ProfileResult.of(profile);
     }
 
     @Override
-    public List<ProfileResult> profileByUserId(User user) {
+    @QueryMapping
+    public List<ProfileResult> profileByUser(@ContextValue("user") User userInput) {
+        User user = UserUtil.require(userInput);
+
         List<Profile> profileList = profileService.getProfileListBy(user);
 
         return profileList.stream().map(ProfileResult::of).toList();
     }
 
     @Override
+    @MutationMapping
     public ProfileResult updateProfile(@Argument @Valid UpdateProfileInput input) {
         Profile profile = profileService.updateProfile(input.toDto());
 
@@ -46,11 +57,23 @@ public class ProfileResolver implements ProfileApi {
     }
 
     @Override
+    @MutationMapping
     public List<ProfileResult> updateProfileSequence(@Argument @Valid List<UpdateProfileSequenceInput> input,
-            User user) {
+            @ContextValue("user") User userInput) {
+        User user = UserUtil.require(userInput);
+
         List<UpdateProfileSequenceDto> dtos = input.stream().map(UpdateProfileSequenceInput::toDto).toList();
         List<Profile> profileList = profileService.updateProfileSequence(dtos, user);
 
         return profileList.stream().map(ProfileResult::of).toList();
+    }
+
+    @Override
+    @MutationMapping
+    public ProfileResult updateProfileColor(@Argument @Valid UpdateProfileColorInput input,
+            @ContextValue("user") User userInput) {
+        UserUtil.require(userInput);
+
+        return ProfileResult.of(profileService.updateProfileColor(input.toDto()));
     }
 }
