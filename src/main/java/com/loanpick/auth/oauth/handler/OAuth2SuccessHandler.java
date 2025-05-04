7f1d74403d4808273e87ee1f4f164d3286b6c120
sanseongKo <tkstjd56@naver.com>
2025-05-04 23:26:41 +0900

    [feature/FP-7] 처음 로그인 시 리프레시 토큰도 함께 리스폰스 할 수 있도록 변경

--- src/main/java/com/loanpick/auth/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/loanpick/auth/oauth/handler/OAuth2SuccessHandler.java
@@ -1,8 +1,13 @@
 package com.loanpick.auth.oauth.handler;
 
+import static com.loanpick.util.Values.ACCESS_TOKEN;
+import static com.loanpick.util.Values.FIFTEEN_MINUTE_MILL;
+import static com.loanpick.util.Values.FOURTEEN_DAYS_SEC;
+import static com.loanpick.util.Values.GRAPHQL_URL;
+import static com.loanpick.util.Values.REFRESH_TOKEN;
+
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
 
@@ -14,11 +19,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loanpick.auth.oauth.constants.HttpHeaderValues;
 import com.loanpick.auth.oauth.handler.reponse.OAuth2Response;
 import com.loanpick.auth.oauth.jwt.JwtProvider;
+import com.loanpick.auth.oauth.service.AuthService;
 import com.loanpick.auth.oauth.service.dto.CustomOAuth2User;
 import com.loanpick.redis.service.CustomRedisService;
 import com.loanpick.user.entity.User;
 import com.loanpick.user.repository.UserRepository;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -31,8 +38,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final UserRepository userRepository;
     private final CustomRedisService customRedisService;
     private final ObjectMapper objectMapper;
-    private static final int FIVE_MINUTE = 300_000;
-    private static final String ACCESS_TOKEN = "access_token";
+    private final AuthService authService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -45,7 +51,11 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         Optional<User> user = userRepository.findByEmail(email);
 
         if (user.isPresent()) {
-            responseSuccessLogin(response, getJwtToken(user.get()));
+            String accessToken = getAccessToken(user.get());
+            String refreshToken = getRefreshToken(user.get());
+            authService.saveRefreshToken(user.get().getId(), refreshToken);
+
+            responseSuccessLogin(response, accessToken, refreshToken);
         } else {
             saveOAuthAuthenticationToRedis(oauthUserId, email, provider);
             responseSignUpUser(response, oauthUserId, provider);
@@ -53,22 +63,36 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     }
 
     private void saveOAuthAuthenticationToRedis(String oauthUserId, String data, String provider) {
-        customRedisService.saveEmailForSignUp(oauthUserId, provider, data, Duration.ofMinutes(FIVE_MINUTE));
+        customRedisService.saveEmailForSignUp(oauthUserId, provider, data, Duration.ofMillis(FIFTEEN_MINUTE_MILL));
     }
 
-    private String getJwtToken(User user) {
-        Date now = new Date();
-        Date expiry = new Date(now.getTime() + FIVE_MINUTE);
-        return jwtProvider.createToken(user, now, expiry);
+    private String getAccessToken(User user) {
+        return jwtProvider.createAccessToken(user);
     }
 
-    private void responseSuccessLogin(HttpServletResponse response, String jwt) throws IOException {
+    private String getRefreshToken(User user) {
+        return jwtProvider.createRefreshToken(user);
+    }
+
+    private void responseSuccessLogin(HttpServletResponse response, String accessToken, String refreshToken)
+            throws IOException {
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(HttpHeaderValues.APPLICATION_JSON);
         response.setCharacterEncoding(HttpHeaderValues.UTF_8);
 
-        String body = objectMapper.writeValueAsString(Map.of(ACCESS_TOKEN, jwt));
+        setRefreshTokenOnCookie(response, refreshToken);
+
+        String body = objectMapper.writeValueAsString(Map.of(ACCESS_TOKEN, accessToken));
         response.getWriter().write(body);
+    }
+
+    private void setRefreshTokenOnCookie(HttpServletResponse response, String refreshToken) {
+        Cookie refreshTokenCookie = new Cookie(REFRESH_TOKEN, refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false);
+        refreshTokenCookie.setPath(GRAPHQL_URL);
+        refreshTokenCookie.setMaxAge(FOURTEEN_DAYS_SEC);
+        response.addCookie(refreshTokenCookie);
     }
 
     private void responseSignUpUser(HttpServletResponse response, String id, String provider) throws IOException {
