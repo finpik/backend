25edc12f6d14980fe946e0b2d801a2ebd71587d5
sanseongKo <tkstjd56@naver.com>
2025-05-06 10:42:20 +0900

    [feature/FP-8] 대출 상품 엔티티 1차 추가

--- src/main/java/com/loanpick/loan/entity/LoanProduct.java
+++ src/main/java/com/loanpick/loan/entity/LoanProduct.java
@@ -1,4 +1,44 @@
 package com.loanpick.loan.entity;
 
+import com.loanpick.loan.entity.enums.LoanPeriodUnit;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * 저장은 대부분의 경우 Excel 저장으로
+ */
+@Entity
 public class LoanProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private float maxInterestRate;
+
+    private float minInterestRate;
+
+    private int maxCreditLine;
+
+    private int loanPeriod;
+
+    @Enumerated(EnumType.STRING)
+    private LoanPeriodUnit loanPeriodUnit;
+
+    private String loanRequirement;
+
+    private String interestRateGuide;
+
+    private String repaymentPeriod;
+
+    private String creditLoanGuide;
+
+    private String loanFeeGuide;
+
+    private String notesOnLoan;
 }
---
+++ src/main/java/com/loanpick/loan/entity/enums/LoanPeriodUnit.java
@@ -0,0 +1,8 @@
+package com.loanpick.loan.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum LoanPeriodUnit {
+    YEAR, MONTH
+}
