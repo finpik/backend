05aafdde6fd9d856ca901ebb950024bc5d60376c
sanseongKo <tkstjd56@naver.com>
2025-05-09 20:53:15 +0900

    [feature/FP-8] 레디스 template에서 -> cache로 변경
    추후 auth 에서도 리팩토링 예정

--- src/main/java/com/loanpick/config/redis/CacheConfig.java
+++
@@ -1,9 +0,0 @@
-package com.loanpick.config.redis;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-public class CacheConfig {
-}
