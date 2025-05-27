33730477f5fa3c5baa86fd8cab9c552187098a4d
sanseongKo <tkstjd56@naver.com>
2025-05-28 00:35:07 +0900

    [Feature/FP-11] 유저가 본 상품 조회 projection 추가

---
+++ finpik-infra/src/main/java/finpik/db/jpa/repository/history/dto/RelatedLoanProductProjection.java
@@ -0,0 +1,10 @@
+package finpik.db.jpa.repository.history.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RelatedLoanProductProjection {
+    private Long productId;
+}
