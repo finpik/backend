060883e734c625a05c851dcd24e6e3b291350989
sanseongKo <tkstjd56@naver.com>
2025-06-03 20:55:36 +0900

    [Feature/FP-17] sse 에미터 이벤트 클래스 변경

--- finpik-common/src/main/java/finpik/dto/RecommendedCompleteEvent.java
+++ finpik-common/src/main/java/finpik/dto/RecommendedCompleteEvent.java
@@ -1,11 +1,36 @@
 package finpik.dto;
 
+import java.util.List;
 import java.util.UUID;
 
-public record RecommendedCompleteEvent(Long profileId, UUID eventId) {
-    public static RecommendedCompleteEvent of(Long profileId) {
-        UUID eventId = UUID.randomUUID();
+//@formatter:off
+public record RecommendedCompleteEvent(
+    Long profileId,
+    UUID eventId,
+    List<RecommendedCompleteEventContent> contentList
+) {
+    public record RecommendedCompleteEventContent(
+        Long loanProductId,
+        String productName,
+        Float minInterestRate,
+        Float maxInterestRate,
+        Integer loanLimitAmount
+    ) {
+        public static RecommendedCompleteEventContent of(
+            Long loanProductId, String productName, Float minInterestRate,
+            Float maxInterestRate, Integer loanLimitAmount
+        ) {
+            return new RecommendedCompleteEventContent(
+                loanProductId,
+                productName,
+                minInterestRate,
+                maxInterestRate,
+                loanLimitAmount
+            );
+        }
+    }
 
-        return new RecommendedCompleteEvent(profileId, eventId);
+    public static RecommendedCompleteEvent of(UUID eventId, Long profileId, List<RecommendedCompleteEventContent> contentList) {
+        return new RecommendedCompleteEvent(profileId, eventId, contentList);
     }
 }
