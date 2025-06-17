581696aabb81e2fe027b7efd8c9195ba44f9e322
sanseongKo <tkstjd56@naver.com>
2025-06-17 18:54:22 +0900

    린트 제거

--- finpik-common/src/main/java/finpik/error/enums/CustomHttpStatus.java
+++ finpik-common/src/main/java/finpik/error/enums/CustomHttpStatus.java
@@ -6,7 +6,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CustomHttpStatus {
-    OK(200), BAD_REQUEST(400), UNAUTHORIZED(401), FORBIDDEN(403), NOT_FOUND(404), INTERNAL_SERVER_ERROR(500);
+    OK(200),
+    BAD_REQUEST(400),
+    UNAUTHORIZED(401),
+    FORBIDDEN(403),
+    NOT_FOUND(404),
+    INTERNAL_SERVER_ERROR(500),
+    ;
 
     private final int value;
 }
