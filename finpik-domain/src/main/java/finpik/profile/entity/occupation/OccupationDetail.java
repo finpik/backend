948ca9c0e558daf2ad39458b12ca0b8233288025
sanseongKo <tkstjd56@naver.com>
2025-06-17 18:52:41 +0900

    [Feature/FP-21] Profile 도메인의 직군이 달라짐에 따라 직군 계층 분리

---
+++ finpik-domain/src/main/java/finpik/profile/entity/occupation/OccupationDetail.java
@@ -0,0 +1,50 @@
+package finpik.profile.entity.occupation;
+
+import finpik.entity.enums.Occupation;
+import finpik.error.enums.ErrorCode;
+import finpik.profile.entity.policy.ProfileCreationSpec;
+
+import static finpik.util.Preconditions.require;
+
+public sealed interface OccupationDetail
+    permits EmployeeDetail, PublicServantDetail, SelfEmployedDetail, FreelancerDetail, OtherDetail
+{
+    Occupation getOccupation();
+    Integer getAnnualIncome();
+
+    static OccupationDetail of(ProfileCreationSpec spec) {
+        require(spec.annualIncome(), ErrorCode.INVALID_ANNUAL_INCOME.getMessage());
+
+        return switch (spec.occupation()) {
+            case EMPLOYEE -> new EmployeeDetail(
+                spec.occupation(),
+                spec.annualIncome(),
+                spec.employmentForm(),
+                spec.employmentDate()
+            );
+
+            case PUBLIC_SERVANT -> new PublicServantDetail(
+                spec.occupation(),
+                spec.annualIncome(),
+                spec.employmentDate()
+            );
+
+            case SELF_EMPLOYED -> new SelfEmployedDetail(
+                spec.occupation(),
+                spec.annualIncome(),
+                spec.businessStartDate()
+            );
+
+            case FREELANCER -> new FreelancerDetail(
+                spec.occupation(),
+                spec.annualIncome(),
+                spec.employmentDate()
+            );
+
+            case OTHER -> new OtherDetail(
+                spec.occupation(),
+                spec.annualIncome()
+            );
+        };
+    }
+}
