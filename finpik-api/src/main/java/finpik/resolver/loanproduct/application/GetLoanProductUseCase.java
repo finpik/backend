e474eb00da7e612a7ce0f605abaf335e3ab2de38
sanseongKo <tkstjd56@naver.com>
2025-07-23 11:12:35 +0900

    [feature/FP-27] 추천 대출 상품 조회 Redis 캐싱 부분 제거

--- finpik-api/src/main/java/finpik/resolver/loanproduct/application/GetLoanProductUseCase.java
+++ finpik-api/src/main/java/finpik/resolver/loanproduct/application/GetLoanProductUseCase.java
@@ -5,9 +5,11 @@ import java.util.List;
 import finpik.resolver.loanproduct.application.dto.LoanProductDto;
 import finpik.resolver.loanproduct.application.dto.RecommendedLoanProductDto;
 import finpik.resolver.loanproduct.application.dto.RelatedLoanProductDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface GetLoanProductUseCase {
-    List<RecommendedLoanProductDto> getRecommendedLoanProducts(Long profileId);
+    Slice<RecommendedLoanProductDto> getRecommendedLoanProductList(Long profileId, Pageable pageable);
 
     LoanProductDto getLoanProduct(Long loanProductId);
 
