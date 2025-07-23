2becf3519113a07c45d1098f88aaf284049e26e9
sanseongKo <tkstjd56@naver.com>
2025-07-23 11:04:46 +0900

    [feature/FP-26] 대출 상품 관련 레파지토리를 loan product repository로 합침

--- finpik-infra/mysql/src/main/java/finpik/repository/loanproduct/LoanProductRepository.java
+++ finpik-infra/mysql/src/main/java/finpik/repository/loanproduct/LoanProductRepository.java
@@ -1,11 +1,22 @@
 package finpik.repository.loanproduct;
 
+import java.util.List;
 import java.util.Optional;
 
 import finpik.LoanProduct;
+import finpik.RecommendedLoanProduct;
+import finpik.RelatedLoanProduct;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanProductRepository {
     Optional<LoanProduct> findByIdWithDescription(Long loanProductId);
+
+    List<RecommendedLoanProduct> saveAllRecommendedLoanProduct(Long profileId, List<RecommendedLoanProduct> recommendedLoanProductList);
+
+    Slice<RecommendedLoanProduct> findAllRecommendedLoanProductSliceByProfileId(Long profileId, Pageable pageable);
+
+    List<RelatedLoanProduct> findAllRelatedLoanProductById(Long loanProductId);
 }
