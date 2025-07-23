a8f36b7f0cbee82598abad18cb4545ceae30bfbc
sanseongKo <tkstjd56@naver.com>
2025-07-23 11:04:46 +0900

    [feature/FP-26] 대출 상품 관련 레파지토리를 loan product repository로 합침

--- finpik-infra/mysql/src/main/java/finpik/repository/loanproduct/impl/LoanProductRepositoryImpl.java
+++ finpik-infra/mysql/src/main/java/finpik/repository/loanproduct/impl/LoanProductRepositoryImpl.java
@@ -1,9 +1,22 @@
 package finpik.repository.loanproduct.impl;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import finpik.LoanProduct;
+import finpik.RecommendedLoanProduct;
+import finpik.RelatedLoanProduct;
+import finpik.entity.loanproduct.RecommendedLoanProductEntity;
+import finpik.jpa.repository.history.userproductview.UserProductViewHistoryJpaRepository;
+import finpik.jpa.repository.history.userproductview.projection.RelatedLoanProductProjection;
 import finpik.jpa.repository.loanproduct.LoanProductJpaRepository;
+import finpik.jpa.repository.loanproduct.RecommendedLoanProductJpaRepository;
+import finpik.jpa.repository.loanproduct.projection.RecommendedLoanProductProjection;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +28,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LoanProductRepositoryImpl implements LoanProductRepository {
     private final LoanProductJpaRepository loanProductJpaRepository;
+    private final RecommendedLoanProductJpaRepository recommendedLoanProductJpaRepository;
+    private final UserProductViewHistoryJpaRepository viewHistoryJpaRepository;
 
     @Override
     @Transactional
@@ -22,5 +37,81 @@ public class LoanProductRepositoryImpl implements LoanProductRepository {
         Optional<LoanProductEntity> loanProductEntity = loanProductJpaRepository.findByIdWithDescription(loanProductId);
 
         return loanProductEntity.map(LoanProductEntity::toDomain);
+    }
+
+    @Override
+    public List<RecommendedLoanProduct> saveAllRecommendedLoanProduct(Long profileId, List<RecommendedLoanProduct> recommendedLoanProductList) {
+        List<RecommendedLoanProductEntity> entityList = recommendedLoanProductList.stream().map(product ->
+            RecommendedLoanProductEntity.of(
+                profileId,
+                product.getLoanProductId(),
+                product.getSimilarity()
+            )
+        ).toList();
+
+        Map<Long, Long> loanProductIdMap = recommendedLoanProductJpaRepository.saveAll(entityList).stream().collect(
+            Collectors.toMap(RecommendedLoanProductEntity::getLoanProductId, RecommendedLoanProductEntity::getId)
+        );
+
+        return recommendedLoanProductList.stream().map(product ->
+            RecommendedLoanProduct.rebuild(
+                loanProductIdMap.get(product.getLoanProductId()),
+                product.getProfileId(),
+                product.getBankName(),
+                product.getLoanProductId(),
+                product.getProductName(),
+                product.getInterestRate().maxInterestRate(),
+                product.getInterestRate().minInterestRate(),
+                product.getMaxLoanLimitAmount(),
+                product.getSimilarity()
+            )
+        ).toList();
+    }
+
+    @Override
+    public Slice<RecommendedLoanProduct> findAllRecommendedLoanProductSliceByProfileId(Long profileId, Pageable pageable) {
+        Slice<RecommendedLoanProductProjection> recommendedLoanProductList =
+            recommendedLoanProductJpaRepository.findAllByProfileId(profileId, pageable);
+
+        List<RecommendedLoanProduct> content = recommendedLoanProductList
+            .stream()
+            .map(product ->
+                RecommendedLoanProduct.rebuild(
+                    product.recommendedLoanProductId(),
+                    product.profileId(),
+                    product.bankName(),
+                    product.loanProductId(),
+                    product.productName(),
+                    product.maxInterestRate(),
+                    product.minInterestRate(),
+                    product.maxLoanLimitAmount(),
+                    product.similarity()
+                )
+            )
+            .toList();
+
+        return new SliceImpl<>(content, pageable, recommendedLoanProductList.hasNext());
+    }
+
+    @Override
+    public List<RelatedLoanProduct> findAllRelatedLoanProductById(Long loanProductId) {
+        List<RelatedLoanProductProjection> projectionList = viewHistoryJpaRepository
+            .getRelatedLoanProductList(loanProductId);
+
+        List<Long> relatedLoanProductIdList = projectionList.stream().map(RelatedLoanProductProjection::loanProductId)
+            .toList();
+
+        List<LoanProductEntity> entityList = loanProductJpaRepository.findAllById(relatedLoanProductIdList);
+
+        return entityList.stream()
+            .map(entity ->
+                RelatedLoanProduct.of(
+                    entity.getId(),
+                    entity.getProductName(),
+                    entity.getMaxInterestRate(),
+                    entity.getMinInterestRate(),
+                    entity.getMaxLoanLimitAmount()
+                )
+            ).toList();
     }
 }
